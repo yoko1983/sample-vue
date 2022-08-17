2400bd447298b9f8b0b03f77f46c2812d91c0b8d
yoko1983 <yoko1983@gmail.com>
2022-08-17 23:03:54 +0900

    display repositories

--- src/App copy.vue
+++ src/App copy.vue
@@ -11,17 +11,26 @@ import { onMounted } from "vue"
 import axios from "axios"
 import { Buffer } from 'buffer'
 
+const token = 'token';
 
-const token: Ref<string> = ref<string>('');
+
+// const headers = {
+//        'Content-Type': 'application/json',
+//        'Authorization': 'Basic id:token'
+// };
+
 
 const results = ref([])
 // const results = ""
 const error = ref([])
 // ボタンクリック時に実行するパターン
-const runapi = (token) => {
-
-
-
+const runapi = () => {
+  axios.get('https://jsonplaceholder.typicode.com/users')
+    .then(response => results.value = response.data)
+    .catch(error => console.log(error))
+};
+// 画面遷移時に実行するパターン
+onMounted(() => {
   axios.get('https://dev.azure.com/yoko1983/test/_apis/git/repositories', 
       { 
             auth: {
@@ -33,21 +42,7 @@ const runapi = (token) => {
       })
     .then(response => results.value = response.data)
     .catch(error => error)
-};
-// 画面遷移時に実行するパターン
-// onMounted(() => {
-//   axios.get('https://dev.azure.com/yoko1983/test/_apis/git/repositories', 
-//       { 
-//             auth: {
-//               password: token
-//             },
-//             params: {
-//               'api-version':'5.1'
-//             }
-//       })
-//     .then(response => results.value = response.data)
-//     .catch(error => error)
-// })
+})
 
 // チェックボックスの状態です
 const checkBoxState = reactive({
@@ -72,35 +67,104 @@ const isButtonEnabled = computed(
 
 const count: Ref<number> = ref<number>(0);
 
+interface User {
+  firstName: string;
+  lastName: string;
+  age: number;
+}
+// const user: User = reactive({
+//   firstName: 'John',
+//   lastName: 'Doe',
+//   age: 25,
+// });
+const user: User = reactive({} as User);
+user.firstName='John'
+user.lastName='Doe'
+user.age=25
 
+const fullName: Ref<string> 
+  = computed<string>(() =>`${user.firstName} ${user.lastName}`);
+
+const changeName = ():void => {
+  user.firstName = "Jane";
+};
+
+const changeName2: (name: string) => void = (name: string):void => {
+  user.firstName = name;
+};
+
+
+// let users: []
+
+// const getAds: () => void = ():void => {
+
+//   axios.get('https://jsonplaceholder.typicode.com/users')
+//             .then(response => users = response.data)
+//             .catch(error => console.log(error))
+
+// };
+
+
+// changeName();
 
 </script>
 
 
 <template>
   <header>
-    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-      <div>
-        token: {{ token }}
-        <ul>
-          <li v-for="repo in results.value">{{ repo.name }}</li>
-        </ul>
-      </div>
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
       <div>
-        <input type="text" v-model="token">
-        <button @click="runapi(token)">
-          Run
+        Count:{{ count }}
+        <button @click="count++">
+          Add Count
         </button>
       </div>
 
+      <div>
+        <button @click="runapi()">
+          Run
+        </button>
+          <ul>
+            <li v-for="repo in results.value">{{ repo.name }}</li>
+          </ul>
+
+          <!-- info:{{ results.value }} -->
+
+          error:{{ error }}
+
+          message:{{ message }}
+      </div>
+
+
+      <div>
+        FullName: {{ fullName }}
+        Count:{{ user.age }}
+        <button @click="user.age++">
+          Add Age
+        </button>
+        <button @click="changeName2('Taro')">
+          ChangeName
+        </button>
+      </div>
+      <div>
+        <label><input type="checkbox" v-model="checkBoxState.aa" />aa</label>
+        <label><input type="checkbox" v-model="checkBoxState.bb" />bb</label>
+        <label><input type="checkbox" v-model="checkBoxState.cc" />cc</label>
+        <label><input type="checkbox" v-model="checkBoxState.dd" />dd</label>
+        <label><input type="checkbox" v-model="checkBoxState.ee" />ee</label>
+        <label><input type="checkbox" v-model="checkBoxState.ff" />ff</label>
+      </div>
+      <button type="button" :disabled="!isButtonEnabled">
+        送信
+      </button>
       <div v-if="hasError">
           <article class="message is-danger">
               <div class="message-header">
